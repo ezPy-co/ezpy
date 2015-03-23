6745bf81eecfce9aa22976fa53f296e42b31bb8d
James Warren <jwarren116@gmail.com>
2015-03-23 13:06:57 -0700

    Register models in admin interface

--- installer/installer_config/admin.py
+++ installer/installer_config/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from installer_config.models import Package, TerminalPrompt, EnvironmentProfile
 
-# Register your models here.
+
+class PackageAdmin(admin.ModelAdmin):
+    model = Package
+    list_display = ('display_name', 'version', 'website')
+
+
+class TerminalPromptAdmin(admin.ModelAdmin):
+    model = TerminalPrompt
+    list_display = ('display_name', 'install_name', 'description')
+
+
+class EnvironmentProfileAdmin(admin.ModelAdmin):
+    model = EnvironmentProfile
+    list_display = ('user', 'packages', 'prompt')
+
+admin.site.register(Package, PackageAdmin)
+admin.site.register(TerminalPrompt, TerminalPromptAdmin)
+admin.site.register(EnvironmentProfile, EnvironmentProfileAdmin)
