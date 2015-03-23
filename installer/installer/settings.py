d370efedeaa387d41e05eb2641616ff5c96e09e8
James Warren <jwarren116@gmail.com>
2015-03-23 11:03:52 -0700

    Place settings in django-configurations classes

--- installer/installer/settings.py
+++ installer/installer/settings.py
@@ -1,84 +1,57 @@
-"""
-Django settings for installer project.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/1.7/topics/settings/
-
-For the full list of settings and their values, see
-https://docs.djangoproject.com/en/1.7/ref/settings/
-"""
-
-# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
 import dj_database_url
-USER_NAME = os.environ.get('USER', '')
-BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+from configurations import Settings
 
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/1.7/howto/deployment/checklist/
+class Base(Settings):
+    USER_NAME = os.environ.get('USER', '')
+    BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+    ALLOWED_HOSTS = []
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'm*lvx#vt8p9+wjcf-r^o_z*b2i7vb7!a7alpkmboozqfp1!u6t'
+    INSTALLED_APPS = (
+        'django.contrib.admin',
+        'django.contrib.auth',
+        'django.contrib.contenttypes',
+        'django.contrib.sessions',
+        'django.contrib.messages',
+        'django.contrib.staticfiles',
+    )
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+    MIDDLEWARE_CLASSES = (
+        'django.contrib.sessions.middleware.SessionMiddleware',
+        'django.middleware.common.CommonMiddleware',
+        'django.middleware.csrf.CsrfViewMiddleware',
+        'django.contrib.auth.middleware.AuthenticationMiddleware',
+        'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
+        'django.contrib.messages.middleware.MessageMiddleware',
+        'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    )
 
-TEMPLATE_DEBUG = True
+    ROOT_URLCONF = 'installer.urls'
+    WSGI_APPLICATION = 'installer.wsgi.application'
 
-ALLOWED_HOSTS = []
+    DATABASES = {
+            'default': dj_database_url.config(
+                default='postgres://{}:@localhost:5432/installer'.format(USER_NAME))
+        }
 
+    LANGUAGE_CODE = 'en-us'
+    TIME_ZONE = 'America/Los_Angeles'
 
-# Application definition
+    USE_I18N = True
+    USE_L10N = True
+    USE_TZ = True
 
-INSTALLED_APPS = (
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
-)
-
-MIDDLEWARE_CLASSES = (
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
-)
-
-ROOT_URLCONF = 'installer.urls'
-
-WSGI_APPLICATION = 'installer.wsgi.application'
-
-
-# Database
-# https://docs.djangoproject.com/en/1.7/ref/settings/#databases
-
-DATABASES = {
-        'default': dj_database_url.config(
-            default='postgres://{}:@localhost:5432/installer'.format(USER_NAME))
-    }
+    STATIC_URL = '/static/'
 
 
-# Internationalization
-# https://docs.djangoproject.com/en/1.7/topics/i18n/
-
-LANGUAGE_CODE = 'en-us'
-
-TIME_ZONE = 'UTC'
-
-USE_I18N = True
-
-USE_L10N = True
-
-USE_TZ = True
+class Dev(Base):
+    DEBUG = True
+    TEMPLATE_DEBUG = DEBUG
+    SECRET_KEY = 'secret'
 
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.7/howto/static-files/
-
-STATIC_URL = '/static/'
+class Prod(Base):
+    DEBUG = False
+    TEMPLATE_DEBUG = DEBUG
+    SECRET_KEY = os.environ.get('SECRET_KEY')
