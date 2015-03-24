743c47374e9ab966d4efe4591861feafe60c853c
James Warren <jwarren116@gmail.com>
2015-03-23 19:52:38 -0700

    Extend profile view; require login to view profile

--- installer/installer_profile/views.py
+++ installer/installer_profile/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from installer_config.models import EnvironmentProfile
 
 
+@login_required()
 def profile(request):
-    return render(request, 'profile.html')
+    user = request.user
+    profiles = EnvironmentProfile.objects.filter(user=user).all()
+    return render(request, 'profile.html', {
+        'user': user,
+        'profiles': profiles,
+    })
