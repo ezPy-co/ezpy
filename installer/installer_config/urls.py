23489922c991684bd18bf1bc99d730779973ca9f
James Warren <jwarren116@gmail.com>
2015-03-23 19:28:19 -0700

    Add DeleteView for Environment Profile

--- installer/installer_config/urls.py
+++ installer/installer_config/urls.py
@@ -1,8 +1,20 @@
-from django.conf.urls import patterns, include, url
+from django.conf.urls import patterns, url
 from installer_config.views import (CreateEnvironmentProfile,
-                                    UpdateEnvironmentProfile)
+                                    UpdateEnvironmentProfile,
+                                    DeleteEnvironmentProfile)
 
-urlpatterns = patterns('',
-    url(r'^create_env/$', CreateEnvironmentProfile.as_view(), name='CreateEnv'),
-    url(r'^update_env/(?P<pk>\d+)/$', UpdateEnvironmentProfile.as_view(), name='UpdateEnv'),
+urlpatterns = patterns(
+    '',
+
+    url(r'^create_env/$',
+        CreateEnvironmentProfile.as_view(),
+        name='CreateEnv'),
+
+    url(r'^update_env/(?P<pk>\d+)/$',
+        UpdateEnvironmentProfile.as_view(),
+        name='UpdateEnv'),
+
+    url(r'^delete_env/(?P<pk>\d+)/$',
+        DeleteEnvironmentProfile.as_view(),
+        name='DeleteEnv'),
 )
