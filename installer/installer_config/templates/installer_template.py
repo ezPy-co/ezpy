f962711880f6f140db63dfc893ce56992f9bba9a
henrykh <henryhowes@gmail.com>
2015-03-24 14:50:57 -0700

    updates to installer_template

--- installer/installer_config/templates/installer_template.py
+++ installer/installer_config/templates/installer_template.py
@@ -6,30 +6,29 @@ from subprocess import call
 # For a straight pip install with no setup
     {% for step in choice.step.all %}
 
-    # Download file
+    
     {% if step.step_type == 'dl' %}
-    print step + "\n"
-    print 'download\n'
+    print {{step.url}}
     {% endif %}
 
-    # Pre execution setup
     {% if step.step_type == 'edprof' %}
     print 'profile change\n'
     {% endif %}
 
-    # Execute file
     {% if step.step_type == 'edfile' %}
     # call(['pip', 'install', option.package_name])
     print 'file change\n'
     {% endif %}
     {% if step.step_type == 'env' %}
-    # Need to know download file name and type...
     print 'set a thing\n'
     {% endif %}
 
-    # Post execution setup
     {% if step.step_type == 'pip' %}
     print 'pip\n'
+    {% endif %}
+
+    {% if step.step_type == 'exec' %}
+    print {{step.args}}
     {% endif %}
 
     {% endfor %}
