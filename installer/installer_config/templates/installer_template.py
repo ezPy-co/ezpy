364bff5c9d7cc818a722aa6fdafd678f804d67aa
Matthew Lee <ml7@uw.edu>
2015-03-24 18:05:57 -0700

    Added spaceless template tag to reduce most of the extra white space ...

---
+++ installer/installer_config/templates/installer_template.py
@@ -0,0 +1,38 @@
+#!usr/bin/env python
+from subprocess import call
+
+
+{% for choice in choices %}
+{% spaceless %}
+# For a straight pip install with no setup
+    {% for step in choice.step.all %}
+{% spaceless %}
+    {% if step.step_type == 'dl' %}
+    print {{step.url}}
+    {% endif %}
+
+    {% if step.step_type == 'edprof' %}
+    print 'profile change\n'
+    {% endif %}
+
+    {% if step.step_type == 'edfile' %}
+    # call(['pip', 'install', option.package_name])
+    print 'file change\n'
+    {% endif %}
+
+    {% if step.step_type == 'env' %}
+    print 'set a thing\n'
+    {% endif %}
+
+    {% if step.step_type == 'pip' %}
+    print 'pip\n'
+    {% endif %}
+
+    {% if step.step_type == 'exec' %}
+    print {{step.args}}
+    {% endif %}
+{% endspaceless %}
+    {% endfor %}
+
+{% endspaceless %}
+{% endfor %}
