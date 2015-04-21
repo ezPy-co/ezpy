d98d73b75d1b20937d0c348a76370585cd9c5d0d
James Warren <jwarren116@gmail.com>
2015-04-21 11:35:01 -0700

    Update tests for changes made to script template

--- installer/installer_config/tests.py
+++ installer/installer_config/tests.py
@@ -173,20 +173,20 @@ class DownloadFileFormationTest(TestCase):
         response = self.client.get(reverse('installer_config:download_profile',
                                    kwargs={'pk': profiles[0].pk}))
         # Verify that choices selected are present
-        self.assertIn('# For choice important thing', response.content)
-        self.assertIn('# For choice your env', response.content)
-        self.assertIn('# For choice get', response.content)
+        self.assertIn('# For choice: important thing', response.content)
+        self.assertIn('# For choice: your env', response.content)
+        self.assertIn('# For choice: get', response.content)
 
         # Check that the steps for choices selected and only choices selected
         # for a given environment are present in the generated python file
         self.assertIn('# Download and run', response.content)
         # self.assertIn('# Edit a file', response.content)
-        self.assertIn('# Edit a profile', response.content)
+        self.assertIn('# Edit .bashrc profile', response.content)
 
         # Verify choices that don't belong are not present
         self.assertNotIn('# Add a key, value pair', response.content)
         self.assertNotIn('"Executing " + \' \'.join(command_line)', response.content)
-        self.assertNotIn('# Pip install, assuming', response.content)
+        self.assertNotIn('# Pip install', response.content)
 
     def test_choice_presence_set2(self):
         self.user.save()
@@ -194,16 +194,16 @@ class DownloadFileFormationTest(TestCase):
         response = self.client.get(reverse('installer_config:download_profile',
                                            kwargs={'pk': profiles[1].pk}))
 
-        self.assertIn('# For choice bash shenanigannns', response.content)
-        self.assertIn('# For choice text editor', response.content)
+        self.assertIn('# For choice: bash shenanigannns', response.content)
+        self.assertIn('# For choice: text editor', response.content)
 
         self.assertIn('# Add a key, value pair', response.content)
         self.assertIn('"Executing " + \' \'.join(command_line)', response.content)
-        self.assertIn('# Pip install, assuming', response.content)
+        self.assertIn('# Pip install', response.content)
 
         self.assertNotIn('# Download and run', response.content)
-        self.assertNotIn('# Edit a file', response.content)
-        self.assertNotIn('# Edit a profile', response.content)
+        self.assertNotIn('# Edit file', response.content)
+        self.assertNotIn('# Edit .bashrc profile', response.content)
 
     def test_choice_presence_set3(self):
         self.user.save()
@@ -211,16 +211,17 @@ class DownloadFileFormationTest(TestCase):
         response = self.client.get(reverse('installer_config:download_profile',
                                            kwargs={'pk': profiles[2].pk}))
 
-        self.assertIn('# For choice a pip package', response.content)
-        self.assertIn('# For choice other', response.content)
+        self.assertIn('# For choice: a pip package', response.content)
+        self.assertIn('# For choice: other', response.content)
 
         # Verify no steps for this set of choices
         self.assertNotIn('# Download and run', response.content)
         self.assertNotIn('# Edit a file', response.content)
-        self.assertNotIn('# Edit a profile', response.content)
+        self.assertNotIn('# Edit .bashrc profile', response.content)
         self.assertNotIn('# Add a key, value pair', response.content)
         self.assertNotIn('"Executing " + \' \'.join(command_line)', response.content)
-        self.assertNotIn('# Pip install, assuming', response.content)
+        self.assertNotIn('# Pip install', response.content)
+
 
 def set_data(user):
     inputs = [('important thing', 'core', 1),
